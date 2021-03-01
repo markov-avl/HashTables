c96a292fdc776252fc6719979a9a675370f0fe88
Andrey Markov <andrewshasucks@gmail.com>
2021-03-01 20:40:01 +1000

    minor changes

--- src/Fridge.cpp
+++ src/Fridge.cpp
@@ -1,1 +1,11 @@
 #include "Fridge.h"
+
+Fridge::Fridge(unsigned int n) {
+    this->size = (n < 1 ? Fridge::defaultSize : n);
+    this->count = 0;
+    this->table = new Product[n];
+}
+
+Fridge::~Fridge() {
+    delete[] this->table;
+}
--- src/Fridge.h
+++ src/Fridge.h
@@ -1,10 +1,32 @@
 #ifndef HASHTABLES_FRIDGE_H
 #define HASHTABLES_FRIDGE_H
+#include <iostream>
+
+
+struct Product {
+    std::string data;
+    bool isFree = true;
+};
 
 
 class Fridge {
-
+private:
+    static const int defaultSize = 256;
+    unsigned int size;
+    unsigned int count;
+    Product* table;
+    unsigned int hash1(Product product);
+    unsigned int hash2(unsigned int address);
+public:
+    explicit Fridge(unsigned int n = Fridge::defaultSize);
+    ~Fridge();
+    int add(Product product);
+    int del(Product product);
+    int search(Product product);
 };
+
+
+std::ostream& operator<<(std::ostream& out, Fridge& fridge);
 
 
 #endif
